9048272eecc4ff673b0b6f4fdb22e2ff837c7a89
Courtney <courtney.mae.phillips@gmail.com>
2015-06-19 09:38:23 -0700

    Add resources for question, update router accordingly.

---
+++ app/controllers/question.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isEditing: false,
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    delete: function() {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('questions')
+      }
+    },
+    save: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.resource('question', {path: 'questions/:question_id'}, function() {
     this.resource('new-answer');
   });
+  this.resource('new-question');
+  this.resource('answers');
 });
-
 export default Router;

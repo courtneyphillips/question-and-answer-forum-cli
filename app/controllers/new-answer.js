ea3a0704caff760787e3392d3915498e0bfe3d37
Courtney <courtney.mae.phillips@gmail.com>
2015-06-19 10:43:16 -0700

    Add ability to save answers to firebase and display appropriately.

--- app/controllers/new-answer.js
+++ app/controllers/new-answer.js
@@ -4,6 +4,8 @@ export default Ember.Controller.extend({
   actions: {
     save: function() {
       var answer = this.get('model');
+      var text = this.get('text');
+      answer.set('text', text);
       answer.save();
       var controller = this;
       answer.get('question').then(function(question) {

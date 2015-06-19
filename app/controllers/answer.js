39bf9a89e969367f69d9ed13618bbe1c876ad5d4
Courtney <courtney.mae.phillips@gmail.com>
2015-06-19 09:58:57 -0700

    Add controller for answers.

---
+++ app/controllers/answer.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    delete: function() {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('question');
+      }
+    }
+  }
+});
